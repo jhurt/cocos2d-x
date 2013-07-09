fed678e3f497883cf5bdefd7faa7c49e2f913aaa
Jason Hurt <jleehurt@gmail.com>
2013-07-09 16:19:48 -0700

    revert to 2.1.3 since 2.1.4 is unstable

--- cocos2dx/CCDirector.h
+++ cocos2dx/CCDirector.h
@@ -225,6 +225,11 @@ public:
      * ONLY call it if there is a running scene.
      */
     void popScene(void);
+    void popSceneTransitionSlideInT(float time);
+    void popSceneTransitionSlideInB(float time);
+    void popSceneTransitionFade(float time);
+    void popSceneTransitionCrossFade(float time);
+
 
     /**Pops out all scenes from the queue until the root scene in the queue.
      * This scene will replace the running one.
